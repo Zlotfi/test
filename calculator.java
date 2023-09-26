07f7b461e26ef9b80df88eaa0cc231bbc87687d5
zlotfi <zlotfi758@gmail.com>
2023-09-26 22:55:29 +0330

    add mul method

--- calculator.java
+++ calculator.java
@@ -2,4 +2,8 @@ public class calculator {
     public int sum(int firstNumber, int secondNumber){
         return firstNumber + secondNumber;
     }
+
+    public int mul(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
 }
