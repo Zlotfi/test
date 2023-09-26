b1681d9a779698c02ea7cd8214b40edf77e43eec
zlotfi <zlotfi758@gmail.com>
2023-09-26 23:08:34 +0330

    add main method for test mul and sum method in calc class

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        calculator cal = new calculator();
+        System.out.println("2 + 2 equals to:");
+        System.out.println(cal.sum(2, 2));
 
+        System.out.println("2 * 2 equals to:");
+        System.out.println(cal.mul(2, 2));
+    }
 }
